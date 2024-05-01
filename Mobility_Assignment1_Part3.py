b369de7f286ddb4b02412c3d545eca8c85af6ce9
WoutervanMinnen <wouter5minnen@gmail.com>
2024-05-01 23:09:46 +0200

    Werkend maar niet zeker of de het ook echt correct is.

--- Mobility_Assignment1_Part3.py
+++ Mobility_Assignment1_Part3.py
@@ -5,16 +5,23 @@ from tabulate import tabulate
 df_base = pd.read_csv('Base_csv.csv')
 df_old = pd.read_csv('Old_csv.csv')
 
-# Filter the data for travdist == 0 and travtime > 0
-filtered_df_base = df_base[(df_base['travdist'] == 0) & (df_base['travtime'] > 0)]
-filtered_df_old = df_old[(df_old['travdist'] == 0) & (df_old['travtime'] > 0)]
+# Convert 'TravDist' and 'TravTime' columns to numeric type, coercing errors to NaN
+df_base['TravDist'] = pd.to_numeric(df_base['TravDist'], errors='coerce')
+df_base['TravTime'] = pd.to_numeric(df_base['TravTime'], errors='coerce')
+
+df_old['TravDist'] = pd.to_numeric(df_old['TravDist'], errors='coerce')
+df_old['TravTime'] = pd.to_numeric(df_old['TravTime'], errors='coerce')
+
+# Filter the data for travdist != 0 and travtime > 0
+filtered_df_base = df_base[(df_base['TravDist'] != 0) & (df_base['TravTime'] > 0)]
+filtered_df_old = df_old[(df_old['TravDist'] != 0) & (df_old['TravTime'] > 0)]
 
 # Calculate the sum of total distance, total travel time, and number of trips
-total_distance_base = filtered_df_base['travdist'].sum()
-total_distance_old = filtered_df_old['travdist'].sum()
+total_distance_base = filtered_df_base['TravDist'].sum()
+total_distance_old = filtered_df_old['TravDist'].sum()
 
-total_time_base = filtered_df_base['travtime'].sum()
-total_time_old = filtered_df_old['travtime'].sum()
+total_time_base = filtered_df_base['TravTime'].sum()
+total_time_old = filtered_df_old['TravTime'].sum()
 
 num_trips_base = len(filtered_df_base)
 num_trips_old = len(filtered_df_old)
