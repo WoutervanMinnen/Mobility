4ee38a363b3746a1ed9e38a6cca3eaf8bca34936
WoutervanMinnen <wouter5minnen@gmail.com>
2024-05-13 16:24:44 +0200

    Assignment2 latex creation
    Now assignment 2 does the same thing as 1 as it creates the table also in a latex format.

--- Mobility_Assignment1_Part2.py
+++ Mobility_Assignment1_Part2.py
@@ -10,8 +10,6 @@ filtered_df_base = df_base[df_base['Actnr'] == 1]
 filtered_df_old = df_old[df_old['Actnr'] == 1]
 
 # Group by age category and gender, and calculate the percentage of population
-# result_SEC = filtered_df_base.groupby(['Age', 'Gend']).size() / filtered_df_base.groupby('Gend').size() * 100
-# result_old_SEC = filtered_df_old.groupby(['Age', 'Gend']).size() / filtered_df_old.groupby('Gend').size() * 100
 result_SEC = filtered_df_base.groupby(['Age', 'SEC']).size() / len(filtered_df_base) * 100
 result_old_SEC = filtered_df_old.groupby(['Age', 'SEC']).size() / len(filtered_df_old) * 100
 
@@ -54,3 +52,48 @@ print(tabulate(result_table_COMP, headers='keys', tablefmt='pretty'))
 print("===================================================================")
 print("------------------------- Old data -------------------------")
 print(tabulate(result_table_old_COMP, headers='keys', tablefmt='pretty'))
+
+# Save the tables to LaTeX format
+result_table_latex = result_table.to_latex(index=True, escape=False, float_format="%.2f")
+result_table_old_latex = result_table_old.to_latex(index=True, escape=False, float_format="%.2f")
+
+# Modify LaTeX code to include lines and remove {lrrr} formatting
+result_table_latex = result_table_latex.replace('\\begin{tabular}{lrrrrr}', '\\begin{table}[htbp]\n\\centering\n\\caption{Base Data}\n\\label{tab:base_data}\n\\begin{tabular}{|c|' + '|'.join(['c']*len(result_table.columns)) + '|}\n')
+result_table_old_latex = result_table_old_latex.replace('\\begin{tabular}{lrrrrr}', '\\begin{table}[htbp]\n\\centering\n\\caption{Old Data}\n\\label{tab:old_data}\n\\begin{tabular}{|c|' + '|'.join(['c']*len(result_table_old.columns)) + '|}\n')
+result_table_latex = result_table_latex.replace('\\toprule', '\hline')
+result_table_latex = result_table_latex.replace('\\midrule', '\hline')
+result_table_latex = result_table_latex.replace('\\bottomrule', '\hline\n\\end{tabular}\n\\end{table}\n')
+
+result_table_old_latex = result_table_old_latex.replace('\\toprule', '\hline')
+result_table_old_latex = result_table_old_latex.replace('\\midrule', '\hline')
+result_table_old_latex = result_table_old_latex.replace('\\bottomrule', '\hline\n\\end{tabular}\n\\end{table}\n')
+
+# Do the same for COMP
+result_table_latex_COMP = result_table_COMP.to_latex(index=True, escape=False, float_format="%.2f")
+result_table_old_latex_COMP = result_table_old_COMP.to_latex(index=True, escape=False, float_format="%.2f")
+
+# Modify LaTeX code to include lines and remove {lrrr} formatting
+result_table_latex_COMP = result_table_latex_COMP.replace('\\begin{tabular}{lrrrrrr}', '\\begin{table}[htbp]\n\\centering\n\\caption{Base Data}\n\\label{tab:base_data}\n\\begin{tabular}{|c|' + '|'.join(['c']*len(result_table_COMP.columns)) + '|}\n')
+result_table_old_latex_COMP = result_table_old_latex_COMP.replace('\\begin{tabular}{lrrrrrr}', '\\begin{table}[htbp]\n\\centering\n\\caption{Old Data}\n\\label{tab:old_data}\n\\begin{tabular}{|c|' + '|'.join(['c']*len(result_table_old_COMP.columns)) + '|}\n')
+result_table_latex_COMP = result_table_latex_COMP.replace('\\toprule', '\hline')
+result_table_latex_COMP = result_table_latex_COMP.replace('\\midrule', '\hline')
+result_table_latex_COMP = result_table_latex_COMP.replace('\\bottomrule', '\hline\n\\end{tabular}\n\\end{table}\n')
+
+result_table_old_latex_COMP = result_table_old_latex_COMP.replace('\\toprule', '\hline')
+result_table_old_latex_COMP = result_table_old_latex_COMP.replace('\\midrule', '\hline')
+result_table_old_latex_COMP = result_table_old_latex_COMP.replace('\\bottomrule', '\hline\n\\end{tabular}\n\\end{table}\n')
+
+# Write the modified LaTeX code to files with the desired names
+with open('Assignment2_base_SEC.tex', 'w') as f:
+    f.write(result_table_latex)
+
+with open('Assignment2_old_SEC.tex', 'w') as f:
+    f.write(result_table_old_latex)
+
+with open('Assignment2_base_COMP.tex', 'w') as f:
+    f.write(result_table_latex_COMP)
+
+with open('Assignment2_old_COMP.tex', 'w') as f:
+    f.write(result_table_old_latex_COMP)
+
+print("Tables saved as Assignment2_base.tex and Assignment2_old.tex")
